3e41cebec868448fdd9ca94ae7f68a2b03d31d7f
skalogerakis <skalogerakis97@gmail.com>
2019-11-18 23:07:23 +0200

    Co-sim passes

--- MyCode/vHLS/algHLS/Optimazation_2/syn/verilog/myFuncAccel_fadd_32ns_32ns_32_5_full_dsp_1.v
+++ MyCode/vHLS/algHLS/Optimazation_2/syn/verilog/myFuncAccel_fadd_32ns_32ns_32_5_full_dsp_1.v
@@ -1,5 +1,5 @@
 // ==============================================================
-// File generated on Mon Nov 18 19:53:45 EET 2019
+// File generated on Mon Nov 18 23:04:28 EET 2019
 // Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2018.3 (64-bit)
 // SW Build 2405991 on Thu Dec  6 23:36:41 MST 2018
 // IP Build 2404404 on Fri Dec  7 01:43:56 MST 2018
